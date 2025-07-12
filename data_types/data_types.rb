d8ed1000a02560722d0a96079530d5643313ea91
Brandon Espina <brandon.r.espina@gmail.com>
2025-07-12 14:24:12 -0500

    basic data_type exploration

---
+++ data_types/data_types.rb
@@ -0,0 +1,19 @@
+# string
+str = "Hello World"
+puts str
+
+# integer
+int = 10
+puts int
+
+# float
+float = 10.5
+puts float
+
+# boolean
+bool = true
+puts bool
+
+# nil
+nil_val = nil
+puts nil_val
---
+++ data_types/discovery.rb
@@ -0,0 +1,18 @@
+# discovery.rb
+
+# static typing test
+int = 10
+puts int
+
+int = "10"
+puts int
+
+# result:
+# 10
+# 10
+# allowed reassignment of variable with different type 
+
+
+
+
+
